bc8baf0600f79966222232f7d32bac3fbdf765df
DK-ops <321Andika@gmail.com>
2022-05-03 23:39:13 +0700

    mixmatch done, need implement buttons.

--- app/src/main/java/com/mobilepro/hautecoute/NavView/ThirdFragment.java
+++ app/src/main/java/com/mobilepro/hautecoute/NavView/ThirdFragment.java
@@ -1,19 +1,23 @@
 package com.mobilepro.hautecoute.NavView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.mobilepro.hautecoute.MixMatchResult;
 import com.mobilepro.hautecoute.R;
+import com.mobilepro.hautecoute.Validation.LoginP;
+import com.mobilepro.hautecoute.Validation.OnBoarding;
 
 
 public class ThirdFragment extends Fragment {
-
-
     public ThirdFragment() {
         // Required empty public constructor
     }
@@ -23,5 +27,8 @@ public class ThirdFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false);
+
     }
+
+
 }
